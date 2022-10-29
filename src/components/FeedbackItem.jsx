184b55100538a8802c481fc3f4b58e6192c141c1
drkspark <madhusudhanrock753@gmail.com>
2022-10-29 22:58:51 +0530

    FrontEnd Done

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,26 +1,27 @@
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import React from "react";
 import Card from "./shared/Card";
-import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
+import { useContext } from "react";
 
 // State is Data
 // useState is one of the main thing used to handle data
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   // [variable having data, function associated with it] = useState(initial value)
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className={"close"} onClick={() => handleDelete(item.id)}>
+      <button className={"close"} onClick={() => deleteFeedback(item.id)}>
         <FaTimes color={"purple"}></FaTimes>
+      </button>
+      <button onClick={() => editFeedback(item)} className={"edit"}>
+        <FaEdit color={"purple"}></FaEdit>
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
-
-FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired,
-};
 
 export default FeedbackItem;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 //Calculate Rating avg
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
   let average =
     feedback.reduce((acc, curr) => {
       return acc + curr.rating;
@@ -17,9 +20,5 @@ function FeedbackStats({ feedback }) {
     </div>
   );
 }
-
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
 
 export default FeedbackStats;
